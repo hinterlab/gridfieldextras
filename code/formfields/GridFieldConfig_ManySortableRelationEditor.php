707bf3b53118383a86f4f3b62a40bfa7d2c10266
Michael Caruana <michael.caruana@internetrix.com.au>
2015-07-02 10:12:52 +1000

    prevent errors when calling object is unsaved, nicer name for toggle-relation button

--- code/formfields/GridFieldConfig_ManySortableRelationEditor.php
+++ code/formfields/GridFieldConfig_ManySortableRelationEditor.php
@@ -5,23 +5,23 @@ class GridFieldConfig_ManySortableRelationEditor extends GridFieldConfig {
 	 *
 	 * @param int $itemsPerPage - How many items per page should show up
 	 */
-	public function __construct($itemsPerPage=null) {
+	public function __construct($itemsPerPage=null,$obj=null) {
 		
 		$this->addComponent(new GridFieldButtonRow('before'));
-		$this->addComponent(new GridFieldAddNewButton('toolbar-header-right'));
-		$this->addComponent(new GridFieldAddExistingAutocompleter('toolbar-header-right'));
 		$this->addComponent(new GridFieldToolbarHeader());
+		$this->addComponent(new GridFieldAddNewButton('toolbar-header-right'));
+		if ($obj && $obj->ID) {
+		    $this->addComponent(new GridFieldOrderableRows('Sort'));
+			$this->addComponent($bb = new GridFieldManyRelationHandler(true,'toolbar-header-right'));
+			$bb->setButtonTitle('TOGGLE_RELATION', 'Choose existing...');
+		}		
+//		$this->addComponent(new GridFieldAddExistingAutocompleter('toolbar-header-right'));
 		$this->addComponent($sort = new GridFieldSortableHeader());
 		$this->addComponent(new GridFieldDataColumns());
 		$this->addComponent(new GridFieldEditButton());
 		$this->addComponent(new GridFieldDeleteAction(true));
-		$this->addComponent(new GridFieldPageCount('toolbar-header-right'));
-		
-		//my custom components
-		$this->addComponent(new GridFieldOrderableRows('Sort'));
-		
+//		$this->addComponent(new GridFieldPageCount('toolbar-header-right'));
 		$this->addComponent($filter = new GridFieldFilterHeader());
-		
 		$this->addComponent($pagination = new GridFieldPaginator($itemsPerPage));
 		$this->addComponent(new GridFieldDetailForm());
 
@@ -30,4 +30,3 @@ class GridFieldConfig_ManySortableRelationEditor extends GridFieldConfig {
 		$pagination->setThrowExceptionOnBadDataType(false);
 	}
 }
-
