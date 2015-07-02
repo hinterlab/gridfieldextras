be8038064b51fce8df6604d3201c5038e1c3e304
Michael Caruana <michael.caruana@internetrix.com.au>
2015-07-02 10:12:49 +1000

    prevent errors when calling object is unsaved

--- code/formfields/GridFieldConfig_ManySortableRecordEditor.php
+++ code/formfields/GridFieldConfig_ManySortableRecordEditor.php
@@ -4,17 +4,19 @@ class GridFieldConfig_ManySortableRecordEditor extends GridFieldConfig {
 	 *
 	 * @param int $itemsPerPage - How many items per page should show up
 	 */
-	public function __construct($itemsPerPage=null) {
+	public function __construct($itemsPerPage=null,$obj=null) {
 
 		$this->addComponent(new GridFieldButtonRow('before'));
-		$this->addComponent(new GridFieldAddNewButton('toolbar-header-right'));
 		$this->addComponent(new GridFieldToolbarHeader());
+		$this->addComponent(new GridFieldAddNewButton('toolbar-header-right'));
 		$this->addComponent($sort = new GridFieldSortableHeader());
 		$this->addComponent(new GridFieldDataColumns());
 		$this->addComponent(new GridFieldEditButton());
 		$this->addComponent(new GridFieldDeleteAction());
-		$this->addComponent(new GridFieldPageCount('toolbar-header-right'));
-		$this->addComponent(new GridFieldOrderableRows('Sort'));
+//		$this->addComponent(new GridFieldPageCount('toolbar-header-right'));
+		if ($obj && $obj->ID) {
+		    $this->addComponent(new GridFieldOrderableRows('Sort'));
+		}
 		$this->addComponent($pagination = new GridFieldPaginator($itemsPerPage));
 		$this->addComponent(new GridFieldDetailForm());
 
