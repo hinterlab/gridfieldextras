013234d1283308fc64dc1e6c3f19cd5999770339
guywatson <guy.watson@internetrix.com.au>
2016-08-24 11:33:19 +1000

    reload the grid

--- javascript/gridfieldleftandmainextras.js
+++ javascript/gridfieldleftandmainextras.js
@@ -1,16 +1,25 @@
 (function($) {
+	var reloadGrid 				= false;
+	var closestGrid 			= null;
+	
 	$.entwine("ss", function($) {
 		$('.cms-edit-form.CMSPageEditController').entwine({
 			onbeforesubmitform: function(){
 				var save = $('.relationhandler-saverel:visible');
 				if(save.length){
+					reloadGrid 	= true;
+					closestGrid = save.closest(".ss-gridfield");
 					save.trigger('click');
-					
-					setTimeout(function(){
-						// I do not think this slows the triggered function down
-					}, 500);
+				}
+			},
+			onaftersubmitform: function(){
+				if(reloadGrid){
+					var id = closestGrid.attr('id');
+					$('#Form_EditForm_Packages.ss-gridfield').entwine('.').entwine('ss').reload();
+					reloadGrid = false;
 				}
 			}
 		});
+		
 	});
 })(jQuery);
