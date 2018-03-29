3fa493284de2961c65584b9fdb705cc02d750faf
drew-ctrack <drew@ctrack.com.au>
2018-03-29 14:24:18 +1100

    29/3/18 jQuery Entwine fix for Linkable

--- javascript/linkfield-extras.js
+++ javascript/linkfield-extras.js
@@ -1,118 +1,88 @@
 jQuery.entwine("linkfield", function($) {
 
-	//get link from editable row holder 'data-link' attribute. This is more accurate way.
-	function loadEditableRowLink(inputDOM){
-		var link = inputDOM.closest('.ss-gridfield-editable-row--row').first().prev().find('[data-link]').data('link');
-		
-		//change action handler 'load' to 'form
-		link = link.replace('/load/', '/form/');
-		
-		return link;
-	}
+  $("input.link").entwine({
+    onmatch: function() {
+      var self = this;
+      this.setDialog(self.siblings('.linkfield-dialog:first'));
 
-	$("input.link").entwine({
-		onmatch: function() {
-			
-			var self = this;
-			this.setDialog(self.siblings('.linkfield-dialog:first'));
+      var formUrl = this.parents('form').attr('action'),
+          formUrlParts = formUrl.split('?'),
+          formUrl = formUrlParts[0],
+          url = formUrl + '/field/' + this.attr('name') + '/LinkFormHTML';
 
-			var formUrl = this.parents('form').attr('action');
-			
-			if(this.attr('name').indexOf('Milkyway_SS_GridFieldUtils_EditableRow_editableRow') > 0){
-				//this field is loaded via Milkyway/EditableRow. Get correct ajax link for it.
-				formUrl = loadEditableRowLink(this);
-			}
-			
-			var	formUrlParts = formUrl.split('?'),
-				formUrl = formUrlParts[0],
-				url = formUrl + '/field/' + this.attr('name') + '/LinkFormHTML';
+      var editButton = self.parent().siblings('.col-buttons').find('a.edit-link');
+      if(editButton.length){
+        $(".linkfield-remove-button").hide();
+        url = editButton.prop('href');
+        url = url.slice(0, - 5); //remove "edit"
+        url = url + '/ItemEditForm/field/' + this.attr('data-title').replace(/ /g,'') + '/LinkFormHTML';
+        self.getDialog().data("grid", self.closest(".ss-gridfield"));
+      }
 
-			var editButton = self.parent().siblings('.col-buttons').find('a.edit-link');
-			if(editButton.length){
-				$(".linkfield-remove-button").hide();
-				url = editButton.prop('href');
-				url = url.slice(0, - 5); //remove "edit"
-				url = url + '/ItemEditForm/field/' + this.attr('data-title').replace(/ /g,'') + '/LinkFormHTML';
-				self.getDialog().data("grid", self.closest(".ss-gridfield"));
-			}
 
-			if(self.val().length){
-				url = url + '?LinkID=' + self.val();
-			}else{
-				url = url + '?LinkID=0';
-			}
+      if(self.val().length){
+        url = url + '?LinkID=' + self.val();
+      }else{
+        url = url + '?LinkID=0';
+      }
 
-			if(typeof formUrlParts[1] !== 'undefined') {
-				url = url + '&' + formUrlParts[1];
-			}
+      if(typeof formUrlParts[1] !== 'undefined') {
+        url = url + '&' + formUrlParts[1];
+      }
 
-			this.setURL(url);
+      this.setURL(url);
 
-			// configure the dialog
-			var windowHeight = $(window).height();
+      // configure the dialog
+      var windowHeight = $(window).height();
 
-			this.getDialog().data("field", this).dialog({
-				autoOpen: 	false,
-				width:   	$(window).width()  * 80 / 100,
-				height:   	$(window).height() * 80 / 100,
-				modal:    	true,
-				title: 		this.data('dialog-title'),
-				position: 	{ my: "center", at: "center", of: window }
-			});
+      this.getDialog().data("field", this).dialog({
+        autoOpen: 	false,
+        width:   	$(window).width()  * 80 / 100,
+        height:   	$(window).height() * 80 / 100,
+        modal:    	true,
+        title: 		this.data('dialog-title'),
+        position: 	{ my: "center", at: "center", of: window },
+        close: function(){
+          self.getDialog().empty();
 
-			// submit button loading state while form is submitting 
-			this.getDialog().on("click", "button", function() {
-				$(this).addClass("loading ui-state-disabled");
-			});
+        }
+      });
 
-			// handle dialog form submission
-			this.getDialog().on("submit", "form", function() {
-				
-				var dlg = self.getDialog().dialog(),
-					options = {};
+      // submit button loading state while form is submitting
+      this.getDialog().on("click", "button", function() {
+        $(this).addClass("loading ui-state-disabled");
+      });
 
-				options.success = function(response) {
-					if($(response).is(".field")) {
-						self.getDialog().empty().dialog("close");
-						var grid = self.getDialog().data("grid");
-						if(grid){
-							grid.entwine('.').entwine('ss').reload();
-						}else{
-							self.parents('.field:first').replaceWith(response);
-						}
-					} else {
-						self.getDialog().html(response);
-					}
-				}
+      // handle dialog form submission
+      this.getDialog().on("submit", "form", function() {
 
-				$(this).ajaxSubmit(options);
+        var dlg = self.getDialog().dialog(),
+            options = {};
 
-				return false;
-			});
-		}
-	});
+        options.success = function(response) {
+          if($(response).is(".field")) {
+            self.getDialog().empty().dialog("close");
+            var grid = self.getDialog().data("grid");
+            if(grid){
+              grid.entwine('.').entwine('ss').reload();
+            }else{
+              self.parents('.field:first').replaceWith(response);
+            }
+          } else {
+            self.getDialog().html(response);
+          }
+        };
 
-	$(".linkfield-remove-button").entwine({
-		onclick: function() {
-			var formUrl = this.parents('form').attr('action'),
-			    thisInput = this.siblings('input:first');
-			    
-			if(thisInput.attr('name').indexOf('Milkyway_SS_GridFieldUtils_EditableRow_editableRow') > 0){
-				//this field is loaded via Milkyway/EditableRow
-				formUrl = loadEditableRowLink(thisInput);
-			}
-			
-			var	formUrlParts = formUrl.split('?'),
-				formUrl = formUrlParts[0],
-				url = encodeURI(formUrl) + '/field/' + thisInput.prop('name') + '/doRemoveLink';
+        $(this).ajaxSubmit(options);
 
-			if(typeof formUrlParts[1] !== 'undefined') {
-				url = url + '&' + formUrlParts[1];
-			}
-			var holder = this.parents('.field:first');
-			this.parents('.middleColumn:first').html("<img src='framework/images/network-save.gif' />");
-			holder.load(url);
-			return false;
-		},
-	});
+        return false;
+      });
+    },
+    onunmatch: function () {
+      var self = this;
+      $('.linkfield-dialog.ui-dialog-content').filter(function(){
+        return self[0] == $(this).data("field")[0];
+      }).remove();
+    }
+  });
 });
