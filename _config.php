b8b0b2aeb8bb1eaea6ac5874b607ffd2ee40e579
Drew Carrette <drew.carrette@internetrix.com.au>
2019-07-03 14:07:11 +1000

    Added expected directory name

--- _config.php
+++ _config.php
@@ -1,8 +1,8 @@
 <?php
 
-define('GRIDFIELDEXTRAS_DIR', 'gridfieldextras');
+define('GRIDFIELDEXTRAS_DIR', 'silverstripe-gridfieldextras');
 
 $dir = basename(dirname(__FILE__));
-if($dir != "gridfieldextras") {
-	user_error('Directory name must be "gridfieldextras" (currently "'.$dir.'")',E_USER_ERROR);
+if($dir != "silverstripe-gridfieldextras") {
+    user_error('Directory name must be "silverstripe-gridfieldextras" (currently "'.$dir.'")',E_USER_ERROR);
 }
